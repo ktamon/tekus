6eba920aea73ad40cdff16175dcdd0e00d31d87a
Catalina Monsalve <ktamonsalve@gmail.com>
2024-08-26 16:59:22 -0500

    añadiendo login

--- cypress/e2e/test/menu.cy.js
+++ cypress/e2e/test/menu.cy.js
@@ -3,8 +3,17 @@
 describe("Probar navegación a través del menú Multimedia", () => {
 
     before(() => {
+        // Visitamos la pagina de inicio de sesion de TeKus
+        cy.visit('https://qalab.invertebrado.co')
+        //Introducimos el Nombre de usuario o correo electronico
+        cy.get('#mat-input-0').type('qatester')
+        //Introducimos la contraseña
+        cy.get('#mat-form-field-label-3 > .ark-mat-label').type('N9j^u9&Hm@dz2Kcs')
+        //Hacemos clic en el botón inicio de sesion
+        cy.get('span.ark-btn-loader > :nth-child(1)').click();
+
         // Visitamos la pagina del dashboard de TeKus
-        cy.visit('https://qalab.invertebrado.co/screens/multimedia')
+        //cy.visit('https://qalab.invertebrado.co/screens/multimedia')
     })
     it('Debería navegar al menú Multimedia y verificar el contenido', () => {
       // Hacemos clic en la opción del menú "Multimedia"
