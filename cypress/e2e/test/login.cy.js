00589bec0541cc7d4e09a8c3783dd511a14cd9da
Catalina Monsalve <ktamonsalve@gmail.com>
2024-08-26 16:51:06 -0500

    correccion errores

--- cypress/e2e/test/login.cy.js
+++ cypress/e2e/test/login.cy.js
@@ -15,16 +15,6 @@ describe("Probar pagina de rigistro TeKus", () => {
         cy.get('span.ark-btn-loader > :nth-child(1)').click();
         // Verificamos que el usuario haya sido redirigido al dashboard de TeKus
         cy.url().should('eq', 'https://qalab.invertebrado.co/screens/dashboard') // => true
-    })
-    it('Debería navegar al menú Multimedia y verificar el contenido', () => {
-        // Hacemos clic en la opción del menú "Multimedia"
-        cy.get(':nth-child(8) > a.ng-star-inserted > .ark-title').contains('Multimedia').click();
-  
-        // Verificamos que la URL cambie a la página de Multimedia
-        cy.url().should('include', '/multimedia');
-  
-        // Verificamos que el contenido de la página de servicios sea el esperado
-        cy.get('h1').should('contain.text', 'Nuestros Servicios');
     }
 )
 })
